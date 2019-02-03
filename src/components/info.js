357ea2d5c019016589d896aaaa595810be7ea556
viktor <kruvv@mail.ru>
2019-02-03 20:54:21 +0300

    Create app on react

---
+++ src/components/info.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Info extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Погодное приложение</h2>
+        <p>Узнай погоду в вашем городе</p>
+      </div>
+    );
+  }
+}
+export default Info;
