d134381849984c3d6d0083a128803c79fc74f82e
viktor <kruvv@mail.ru>
2019-02-08 19:49:26 +0300

    Add components (form, weather)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
-import Info from "./components/info"
+import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
+
+const API_KEY = "";
 
 class App extends React.Component {
+
+ // Этот метод получает информацию от сервера погоды и преобразует его в json
+  gettingWeather = async() => {
+    const api_url = await
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=${API_KEY}&units=metric`);
+    const data = await api_url.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div>
-        <Info />
+      <Info />
+      <Form />
+      <Weather />
       </div>
     );
   }
