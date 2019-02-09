5babd017edddaefb183d2fd2f7ee22733b9a3d1c
viktor <kruvv@mail.ru>
2019-02-09 12:29:19 +0300

    Создание формы и работа со свойствами

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Info from "./components/info";
 import Form from "./components/form";
 import Weather from "./components/weather";
 
-const API_KEY = "";
+const API_KEY = "82b797b6ebc625032318e16f1b42c016";
 
 class App extends React.Component {
 
  // Этот метод получает информацию от сервера погоды и преобразует его в json
-  gettingWeather = async() => {
+  gettingWeather = async(event) => {
+    event.preventDefault();
+    const city = event.target.elements.city.value;
     const api_url = await
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=${API_KEY}&units=metric`);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
     const data = await api_url.json();
     console.log(data);
   }
@@ -19,7 +21,7 @@ class App extends React.Component {
     return (
       <div>
       <Info />
-      <Form />
+      <Form weatherMethod={this.gettingWeather} />
       <Weather />
       </div>
     );
