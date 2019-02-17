b2e688e1ba6199c15eacfe2b8d679ab9640b04e6
viktor <kruvv@mail.ru>
2019-02-17 19:23:59 +0300

    Updates

---
+++ precache-manifest.eb278511de4044a3a835f98b28945f38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/cities/static/media/polygon.cc54c19b.jpg"
+  },
+  {
+    "revision": "32a4f0bd750128352637",
+    "url": "/cities/static/js/runtime~main.32a4f0bd.js"
+  },
+  {
+    "revision": "92e05b45c44b4ec53667",
+    "url": "/cities/static/js/main.92e05b45.chunk.js"
+  },
+  {
+    "revision": "3929efa2a26948f905df",
+    "url": "/cities/static/js/1.3929efa2.chunk.js"
+  },
+  {
+    "revision": "92e05b45c44b4ec53667",
+    "url": "/cities/static/css/main.b3da563a.chunk.css"
+  },
+  {
+    "revision": "3929efa2a26948f905df",
+    "url": "/cities/static/css/1.e57835fe.chunk.css"
+  },
+  {
+    "revision": "a0412abbe12669b39e80973091945d6b",
+    "url": "/cities/index.html"
+  }
+];
